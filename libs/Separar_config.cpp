ffd4f0649f739aa9f8ebfeaa04f6025835d1b9ff
Yeo. <159602042+11112001@users.noreply.github.com>
2025-08-10 10:04:56 -0500

    Update Separar_config.cpp

--- libs/Separar_config.cpp
+++ libs/Separar_config.cpp
@@ -1,7 +1,8 @@
+#include <cstdlib>
 #include <string>
 #include <fstream>
 #include "Config.h"
-//#include "libs/Separar_Pacientes.cpp"
+#include <iostream>
 
 using namespace std;
 void configAStruct(char** data, Configuracion& config )
@@ -9,18 +10,42 @@ void configAStruct(char** data, Configuracion& config )
     config.tipoSensor = *data[0];
     config.min = stod(data[1]);
     config.max = stod(data[2]);
-
 }
 
-void cargarConfiguracion(const string& archivo) 
+void imprimirEstruct(Configuracion*& config)
+{
+    for (int i = 0; i < 5; i ++)
+    {
+        cout <<"Tipo Sensor: " << config[i].tipoSensor << "\n";
+        cout <<"Minimo " << config[i].min << "\n";
+    }
+}
+
+Configuracion cargarConfiguracion(const string& archivo) 
 {
     ifstream file(archivo);
+    
+    if(!file)
+    {
+        cout << "Error al abrir el archivo" << "\n";
+        exit(0);
+    }
+
     string linea;
-    Configuracion config;
+    Configuracion *configu = new Configuracion[5];
+    int contador = 0;
     
     while (getline(file, linea)) 
     {
-        //configAStruct(separar(linea, 3, ','), config);
+        configAStruct(separar(linea, 3, ','), configu[contador]);
         // Almacenar la configuración en una estructura o hacer algo con ella
+        contador ++;
     }
+
+    imprimirEstruct(configu);
+
+    return *configu;
 }
+
+
+
