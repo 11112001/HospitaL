8165cb3b5c2df2b1eac5302af12f811bbcd15509
Yeo. <159602042+11112001@users.noreply.github.com>
2025-08-13 17:26:32 -0500

    Update creacionRerpote.cpp

--- libs/creacionRerpote.cpp
+++ libs/creacionRerpote.cpp
@@ -1,14 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <cstring>
+//#include <mutex>
 #include <sstream>
 
 //#include "Reporte.h"
 #include "Config.h"
 #include "Reporte.h"
 #include "SalaUCI.h"
-
-
 
 struct FechaR {
     int dia;
@@ -20,16 +19,26 @@ struct FechaR {
 };
 
 using namespace std;
+
 int temp;
 int contador=0; 
 
-void copychar(char *d, const char *ori,int tam)
+/*
+void copychar(char *d, const char *ori, int tam)
 {
-   for(int i=0;i<tam;i++)
-   {
+    for(int i=0;i<tam;i++)
+    {
         d[i]=ori[i];
-   }
+    }
+    
 }
+*/
+
+void copychar(char *d, const char *ori, int tam) {
+    memcpy(d, ori, tam);
+    d[tam] = '\0';           
+}
+
 int  compararChar(const char *a, const char *b, int tam)
 {
     for (int i=0; i<tam;i++)
@@ -42,6 +51,7 @@ int  compararChar(const char *a, const char *b, int tam)
          }
     return 1; 
 }
+
 int Rango(double val, Configuracion *conf, char Tipo)
 {  
    for (int j = 0; j < 5; j++) 
@@ -63,7 +73,7 @@ void imprimirR(ReporteText *rr)
 { for (int i =0; i<10; i++)
     cout << rr[i].RidPac << " " << rr[i].Rfecha << " " << rr[i].RtipoSensor << "  " 
          << rr[i].RValorMed << "  " << rr[i].Rresultado <<"\n";
- }
+}
 
 
 
@@ -78,6 +88,9 @@ FechaR parsearFechaR(const string& fechaStr) {
     
     return f;
 }
+
+//Función para comparar fechas
+
 bool compararFechas(char *fecha1, char* fecha2) 
 {
     //string fechaTexto = "04/08/2025 15:30:45";
@@ -113,6 +126,25 @@ bool compararFechas(char *fecha1, char* fecha2)
         return true; // atp. som iguales, pero se toma cualquiera los dos como mayor.
     }
 }
+
+void ordenarPorTipo(ReporteText *repord)
+{
+    ReporteText tempR;
+
+    for (int i=0;i<contador;i++ )
+    {
+        for (int j=i+1; j<contador;j++)
+        {
+          if (repord[i].RtipoSensor > repord[j].RtipoSensor) 
+          { 
+            tempR = repord[i];
+            repord[i]=repord[j];
+            repord[j]=tempR;
+          }
+       }
+    }
+}
+
 void ordenar(ReporteText *repord)
 {
     ReporteText tempR;
@@ -121,14 +153,17 @@ void ordenar(ReporteText *repord)
     {
         for (int j=i+1; j<contador;j++)
         {
-          if (compararFechas(repord[i].Rfecha, repord[j].Rfecha))
-          { tempR = repord[i];
+          if (compararFechas(repord[i].Rfecha, repord[j].Rfecha) == true)
+          { 
+            tempR = repord[i];
             repord[i]=repord[j];
             repord[j]=tempR;
           }
        }
     }
 }
+
+
 void llenarArchivoTXT(ReporteText *rep)
 {   cout << "Creando reporte" << "  " << contador << " \n";  
     ofstream TXTreporte("Reporte.txt");
@@ -138,24 +173,89 @@ void llenarArchivoTXT(ReporteText *rep)
         cout << "No se pudo crear el archivo \n";
     }
 
+    char *fcor = new char [10];
+    char *fcorDos = new char [10];
+
+    
+    copychar(fcor, rep[0].Rfecha,  10);
+    double acum = 0;
+    int cc = 0;
+
     TXTreporte << "----------------------------------------------------------------\n";
     TXTreporte << "Id Paciente | Fecha | Tipo Sensor | Valor Medicion | Resultados \n";
     TXTreporte << "----------------------------------------------------------------\n";
-    for (int i=0;i<contador;i++)    
-    {
+
+    //fcorDos = fcor; 
+    copychar(fcorDos, fcor, 10);
+
+    double minimo;
+    double maximo;
+
+    minimo = rep[0].RValorMed;
+    maximo = rep[0].RValorMed;
+
+    for (int i=0; i<contador; ++i)    
+    {      
+        if(strncmp(fcor, fcorDos, 10) == 0)
+        {
+            if(minimo > rep[i].RValorMed)
+            {
+                minimo = rep[i].RValorMed;
+            };
+
+            if(maximo < rep[i].RValorMed)
+            {
+                maximo = rep[i].RValorMed;
+            };
+
+            acum = rep[i].RValorMed + acum;
+            cc ++;
+            copychar(fcorDos, rep[i].Rfecha, 10);
+            
+        } else {
+            double prom = acum / cc;
+            
+            TXTreporte << "----------------------------------------------------------------\n";
+            TXTreporte << "        PROMEDIO    " << prom << " Maximo: " << maximo <<" minimo " << minimo<< "\n"; 
+            TXTreporte << "----------------------------------------------------------------\n";
+            
+            minimo = rep[i].RValorMed;
+            maximo = rep[i].RValorMed;
+            
+            acum = 0;
+            cc = 1;
+            copychar(fcor, rep[i].Rfecha, 10);
+
+        }    
+
         TXTreporte << rep[i].RidPac <<"  |  " << rep[i].Rfecha << "  | " << rep[i].RtipoSensor << "  |  " << rep[i].RValorMed << "  | " << rep[i].Rresultado << "\n";
+        
+        
     };
+    if (cc > 0) 
+    {
+        double prom = acum / cc;
+        TXTreporte << "----------------------------------------------------------------\n";
+        TXTreporte << "        PROMEDIO      " << prom << " Maximo: " << maximo <<" minimo " << minimo<< "\n"; 
+        TXTreporte << "----------------------------------------------------------------\n";
+    }
+    
+    
+    
 }
+
 void completarReporte(ReporteText *rep)
 {
-      imprimirR(rep);
-      cout << "Ordenando reporte.........por fecha" << "\0";
-      ordenar(rep);
+      //imprimirR(rep);
+      cout << "Ordenando reporte.........por fecha" << "\n";
+      ordenar(rep); // ordenar por fecha
+      //ordenarPorTipo(rep); // ordenar por Tipo de Sensor
       llenarArchivoTXT(rep);
+}
 
+//Función para llenar la estructura de un reporte
 
- }  
-void llenarReporteText(SalaUCI salauci, ReporteText *report, const char id[11], Configuracion *conf)
+void llenarEstructReporte(SalaUCI salauci, ReporteText *report, const char id[11], Configuracion *conf)
 {
      // contador de registros para el archivo de texto.
     for(int i = 0; i < static_cast<int>(salauci.numMaquinas); i ++)
@@ -172,9 +272,9 @@ void llenarReporteText(SalaUCI salauci, ReporteText *report, const char id[11], 
                    double tempVal = salauci.maquinas[i].mediciones[r].lecturas[a].valor; 
                    char tempTipo = salauci.maquinas[i].mediciones[r].lecturas[a].tipoSensor;
                 
+                   char resultado[10];
                    if(salauci.maquinas[i].mediciones[r].lecturas[a].tipoSensor != 'E')
                     {
-                        char resultado[10];
                         temp = Rango(tempVal, conf, tempTipo);
                         
                         if(temp != 9)
@@ -182,9 +282,14 @@ void llenarReporteText(SalaUCI salauci, ReporteText *report, const char id[11], 
                             strcpy(resultado,"Anomalía");
                            
                         } else 
-                           { strcpy(resultado,"Normal");
+                           { 
+                            strcpy(resultado,"Normal");
                            }
 
+                    }else {
+                        strcpy(resultado, " ");
+                    }
+                    
                         
                         //Pasar datpos a la estructura temporal de repor
                         copychar(report[contador].RidPac,id,11);
@@ -192,23 +297,16 @@ void llenarReporteText(SalaUCI salauci, ReporteText *report, const char id[11], 
                         report[contador].RtipoSensor=tempTipo;
                         report[contador].RValorMed=tempVal;
                         copychar(report[contador].Rresultado,resultado,10);
-
-                        contador++;
                     
-                    
-                    
-                    }
+                        contador++; //Numero de registros pasados al struct reporte
                 }
             }
         }
     }   
     completarReporte(report);
-
 }
 
 void calcular(SalaUCI sala, ReporteText *&reporte,  const char id[11],Configuracion *config)
 {
-    
-
-    llenarReporteText(sala, reporte, id, config);
+    llenarEstructReporte(sala, reporte, id, config);
 }
