1ea0b55141ed623a606f445a9d92a8d24ce81ea4
Yeo. <159602042+11112001@users.noreply.github.com>
2025-08-13 17:25:53 -0500

    Update DeteccionAnomilia.cpp

--- libs/DeteccionAnomilia.cpp
+++ libs/DeteccionAnomilia.cpp
@@ -2,7 +2,6 @@
 #include "Medicion.h"
 #include "MaquinaUCI.h"
 #include "SalaUCI.h"
-//#include <cstdlib>
 #include <iostream>
 
 
@@ -10,7 +9,6 @@ using namespace std;
 
 int estaEnRango(double val, Configuracion *conf, char Tipo)
 {  
-    cout <<"Está en rango";
 
     for (int j = 0; j < 5; j++) 
     {
@@ -18,12 +16,10 @@ int estaEnRango(double val, Configuracion *conf, char Tipo)
         {
             if (val < conf[j].min || val > conf[j].max)
             {
-                // Devolvemos el índice 'j' donde encontramos la anomalía.
                 return j;
             }
         }
     }
-    // Si no se encuentra anomalía, devolvemos 9.
     return 9;
 }
 
@@ -36,6 +32,8 @@ void imprimirConfiguracion(Configuracion*& config)
     }
 }
 
+//Función para detectar anomalía. 
+
 bool deteccionAnomala(const char *idPac, SalaUCI sal, Configuracion*& conf)
 {
     int temp;
@@ -46,27 +44,25 @@ bool deteccionAnomala(const char *idPac, SalaUCI sal, Configuracion*& conf)
         
         for (int r = 0; r < sal.maquinas[i].numMediciones; r++)
         {
-            if(idPac == sal.maquinas[i].mediciones->idPaciente)  //Validación para saber si la ID es correcta.
+            int k = compararChar(idPac, sal.maquinas[i].mediciones->idPaciente, 11);
+            if(k == 1)  //Validación para saber si la ID es correcta. 
             {   
                 for(int a = 0; a < sal.maquinas[i].mediciones[r].numLecturas; a++)
-                {
-                    cout << sal.maquinas[i].mediciones[r].lecturas[a].tipoSensor << "\n";
-                    
+                {                    
                     if(sal.maquinas[i].mediciones[r].lecturas[a].tipoSensor != 'E')
                     {
                         double tempVal = sal.maquinas[i].mediciones[r].lecturas[a].valor; 
                         char tempTipo = sal.maquinas[i].mediciones[r].lecturas[a].tipoSensor;
                         
-                        //imprimirConfiguracion(conf);
-                        
-                        //temp = estaEnRango(sal.maquinas[i].mediciones[r].lecturas[a].valor, conf, sal.maquinas[i].mediciones[r].lecturas[a].tipoSensor);
                         temp = estaEnRango(tempVal, conf,tempTipo);
                         
                         if(temp != 9)
                         {
+                            cout << "--------------------------------------------------\n";
                             cout << "El paciente con id: " << sal.maquinas[i].mediciones[r].idPaciente << " tiene una anomalía en: " 
                             <<  sal.maquinas[i].mediciones[r].lecturas[a].tipoSensor << " con el valor de: " << sal.maquinas[i].mediciones[r].lecturas[a].valor
-                            << " en el rango limite máximo de: " << conf[temp].max << " y rango minimo de: " << conf[temp].min;
+                            << " en el rango limite máximo de: " << conf[temp].max << " y rango minimo de: " << conf[temp].min << "\n";
+                            cout << "--------------------------------------------------\n";                          
                         }
                     }
                 }
