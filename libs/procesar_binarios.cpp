e00d6ce123853f8468f62f9274de653ff9e9f93e
Yeo. <159602042+11112001@users.noreply.github.com>
2025-08-13 17:25:21 -0500

    Update procesar_binarios.cpp

--- libs/procesar_binarios.cpp
+++ libs/procesar_binarios.cpp
@@ -15,6 +15,8 @@ void leerDato(ifstream& ar, void* destino, size_t tama)
     ar.read(reinterpret_cast<char*>(destino), tama);
 }
 
+//Función para leer el archivo .bsf
+
 SalaUCI leerEstructuraBinaria(string Ruta)
 {
     
@@ -34,9 +36,6 @@ SalaUCI leerEstructuraBinaria(string Ruta)
     sala.idSala = sala.idSala;
     sala.numMaquinas = sala.numMaquinas;
 
-    cout << "Sala: " << static_cast<int>(sala.idSala) << "\n";
-    cout << "Numero Maquinas: " << static_cast<int>(sala.numMaquinas) << "\n";
-
     //Si sala.num_maquinas es mayor que 0, reservar un arreglo dinámico de Maquina de ese tamaño; si no, poner el puntero a nullptr
     sala.maquinas = (sala.numMaquinas ? new MaquinaUCI[sala.numMaquinas] : nullptr);
 
@@ -51,16 +50,6 @@ SalaUCI leerEstructuraBinaria(string Ruta)
         
         sala.maquinas[i].idMaquina = static_cast<int>(maq.idMaquina);
         sala.maquinas[i].numMediciones = maq.numMediciones;
-
-        cout << "Sala xx: " << static_cast<int>(maq.idMaquina) << "\n";
-        cout << "Numero Mediciones Xx" << sala.maquinas[i].numMediciones << "\n";
-
-        //Leer valores desde el binario
-        cout << "\n--- Máquina " << (i+1) << " ---\n";
-        cout << "ID Maquina: " << static_cast<int>(sala.maquinas[i].idMaquina) << "\n";
-        cout << "Número de mediciones: " << maq.numMediciones << "\n";
-        cout << "\n --------------- \n";
-        
         
         maq.mediciones = (maq.numMediciones ? new Medicion[maq.numMediciones] : nullptr);    
         
@@ -76,15 +65,6 @@ SalaUCI leerEstructuraBinaria(string Ruta)
             strcpy(sala.maquinas[i].mediciones[m].fechaHora, med.fechaHora);
             sala.maquinas[i].mediciones[m].numLecturas = med.numLecturas;
             
-            cout << "Id paciente Xx " << sala.maquinas[i].mediciones[m].idPaciente << "\n";
-            cout << "Fecha Hora Xx " << sala.maquinas[i].mediciones[m].fechaHora << "\n";
-            cout << "Mediciones Xx " << sala.maquinas[i].mediciones[m].numLecturas << "\n";
-
-            cout << "\n  >> Medición " << (m+1) << "\n";
-            cout << "  ID Paciente: " << med.idPaciente << "\n";
-            cout << "  Fecha y hora: " << med.fechaHora << "\n";
-            cout << "  Número de lecturas: " << med.numLecturas << "\n";
-            
             med.lecturas = (med.numLecturas ? new Lectura[med.numLecturas] : nullptr);
 
             for (int r = 0; r < med.numLecturas; ++r)
@@ -93,7 +73,6 @@ SalaUCI leerEstructuraBinaria(string Ruta)
                 leerDato(archivo, &L.tipoSensor, 1);
                 sala.maquinas[i].mediciones[m].lecturas[r].tipoSensor = L.tipoSensor;
                 
-                cout <<"tipo Sensor Xx " << sala.maquinas[i].mediciones[m].lecturas[r].tipoSensor << "\n";
 
                 char dato[8];
                 if(L.tipoSensor == 'P')
@@ -106,10 +85,6 @@ SalaUCI leerEstructuraBinaria(string Ruta)
 
                 sala.maquinas[i].mediciones[m].lecturas[r].valor = L.valor;
 
-                cout <<"Valor xx " << sala.maquinas[i].mediciones[m].lecturas[r].valor << "\n";
-
-                cout << "  Lectura " << (r + 1) << "\n";
-                cout << "  Tipo: " << L.tipoSensor << "\n";
                 if(L.tipoSensor == 'P')
                 {
                     cout << "  Valor: " << dato << "\n";    
@@ -117,12 +92,8 @@ SalaUCI leerEstructuraBinaria(string Ruta)
                     cout << "  Valor: " << L.valor << "\n";
                 }
             }
-            //memset(&maq, 0, sizeof(maq));
         }
     }
 
-    //cout << "Sala dentro de leer binario: " << static_cast<int>(sala.maquinas[0].idMaquina) << "\n";
-    //cout << "otro dato: " << sala.maquinas[0].mediciones[0].numLecturas <<"\n";
     return sala;
 }
-//
